17b806df948ef955592f501a4d768b7e8644ca9c
Sri Gustinah Fauziah <138380779+srigstinahf@users.noreply.github.com>
2023-11-16 20:31:07 +0700

    Update Jawaban6.sql

--- UTS/Jawaban6.sql
+++ UTS/Jawaban6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT_WS(' ', m.nama_depan, m.nama_belakang) AS mahasiswa, k.`nama`AS mata_kuliah, CONCAT_WS(' ', d.nama_depan, d.nama_belakang) AS dosen, CONCAT_WS(' ', d.kode_dosen) AS dosen
+SELECT CONCAT_WS(' ', m.nama_depan, m.nama_belakang) AS mahasiswa, k.`nama`AS mata_kuliah, CONCAT_WS(' ', d.nama_depan, d.nama_belakang) AS dosen, CONCAT_WS(' ', d.kode_dosen) AS kode_dosen
 FROM mahasiswa AS m
 INNER JOIN mata_kuliah AS k ON k.id = m.mata_kuliah_id
 LEFT JOIN dosen AS d ON d.mata_kuliah_id = k.id
